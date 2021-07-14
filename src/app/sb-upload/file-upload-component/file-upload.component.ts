dda54ee5ce1f96675de37066a9851417672d66a7
pravin <pravinpgr9@gmail.com>
2021-07-14 19:44:50 +0530

    Integration of Material UI angular theme on upload component

--- src/app/sb-upload/file-upload-component/file-upload.component.ts
+++ src/app/sb-upload/file-upload-component/file-upload.component.ts
@@ -4,6 +4,8 @@ import { Message } from 'primeng/primeng';
 import { SbAuthenticationService } from '../../auth/sb-authentication.service';
 import {AppConfig} from "../../config/app.config";
 
+import {MatSnackBar} from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-file-upload-component',
   templateUrl: './file-upload.component.html',
@@ -16,16 +18,24 @@ export class FileUploadComponent implements OnInit, OnDestroy {
 
   uploadedFiles: any[] = [];
 
+  durationInSeconds = 5;
+
   constructor(
     private breadcrumbService: BreadcrumbService,
     private authenticationService: SbAuthenticationService,
-    private appConfig: AppConfig
+    private appConfig: AppConfig,private _snackBar: MatSnackBar
   ) {
     this.destination = `${appConfig.gatewayBaseUrl}/csv`;
     this.breadcrumbService.setItems([
       { label: 'Portfolio' },
       { label: 'File Upload' }
     ]);
+  }
+
+  openSnackBar() {
+    this._snackBar.openFromComponent(PizzaPartyComponent, {
+      duration: this.durationInSeconds * 1000,
+    });
   }
 
   ngOnInit() { }
@@ -37,6 +47,7 @@ export class FileUploadComponent implements OnInit, OnDestroy {
 
     this.msgs = [];
     this.msgs.push({ severity: 'info', summary: 'File Uploaded', detail: '' });
+    this.openSnackBar();
   }
 
   onBeforeSend(event) {
@@ -49,3 +60,14 @@ export class FileUploadComponent implements OnInit, OnDestroy {
 
   onError(event) { }
 }
+
+@Component({
+  selector: 'snack-bar-component-example-snack',
+  templateUrl: 'snack-bar-component-example-snack.html',
+  styles: [`
+    .example-pizza-party {
+      color: hotpink;
+    }
+  `],
+})
+export class PizzaPartyComponent {}
